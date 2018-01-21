a8e1ebb3a55c241bffb385191aacf99661b45617
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-01-21 11:18:02 +0530

    Create even number.c

---
+++ even number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int counter;
+printf("Even number between 1 to 50\n");
+for(counter=1;counter<=50;counter++);
+if(cout%2==0)
+{
+ printf("%d",counter);
+ }
+ }
+ return 0;
+ }
