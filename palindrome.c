7eb65b726fcfda95b6ed6083755d924d5580a21e
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-01-21 10:29:52 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void ispalindrome(charstr[])
+{
+int l=0;
+while(h>1)
+{
+if(str[i++]!=str[h--]
+{
+printf("%is not palindrome",string);
+return,
+}
+}
+printf("% is palindrome",str);
+}
+int main
+{
+ is palindrome("madam")
+ is palindromr("aabb")
+ is palindrome("nnnn")
+ return 0;
+ }
