a6b6dc4856eff1f2753bf1d64d34b150016f0035
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-01-21 11:04:48 +0530

    Create odd number.c

---
+++ odd number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int num,m=20,n=40;
+clrscr();
+printf("print odd number in a given range mto n:\n");
+printf(number=m;mum<=n;num++)
+{
+if(number%2==1)
+printf("%d",num);
+}
+getch();
+}
