9430f36a11eac62ca9e4e4f4bc143a90d0ac20d1
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-01-21 11:31:45 +0530

    Update even number.c

--- even number.c
+++ even number.c
@@ -3,7 +3,7 @@ int main()
 {
 int counter;
 printf("Even number between 1 to 50\n");
-for(counter=1;counter<=50;counter++);
+for(counter=1;counter<=50;counter++)
 if(cout%2==0)
 {
  printf("%d",counter);
