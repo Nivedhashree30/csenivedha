eac069baa8a7a8948ae8674cfefbdc27f4360b48
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-01-23 16:18:27 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+ unsigned long long factorial=1;
+printf("Enter an integer:");
+scanf("Enter an integer:");
+if(n<0)
+printf("Error!Factorial of a negative number does not exist:");
+else
+{
+ for(i=1;i<=n;
+++i)
+{
+factorial=1;
+}
+printf("Factorial of %d=%d\n",n fact);
+}
+return 0;
+}
