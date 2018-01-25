cd2977cec640f6605e0045dbee84301b224aa21b
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-01-25 16:33:35 +0530

    Create maximum array.c

---
+++ maximum array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+int i,array[50],number;
+
+printf("|n Enter no of element");
+Scanf("%d",&number)
+printf("\n Enter the value:");
+{
+for(i=0;i<=number;i++)
+{
+ scanf(%d", & array[i]);
+ }
+ for(i=0;i<=number;i++)
+ printf("\n array{%d]=%d",i=array[i]);
+ }
+ return(0)
+ }
