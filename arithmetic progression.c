2a12f602445e3e0704d53f05f285579a45864eda
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-01-25 16:21:51 +0530

    Create arithmetic progression.c

---
+++ arithmetic progression.c
@@ -0,0 +1,16 @@
+#include,stdio.h>
+#include<stdlib.h>
+int main()
+int first,difference,terms,values,sum=0,i;
+printf("Enter the number of terms in Ap series\n");
+scanf(%d%d",&first, &difference);
+value=first;
+printf("AP SERIES\n");
+for(i=0;i<terms;i++)
+sum+=value;
+value=value+difference;
+}
+printf("\n Sum of AP series till %d terms is %d \n" from sum0;
+getch();
+return 0:
+}
