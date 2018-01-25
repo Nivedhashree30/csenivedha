e763a32df00e747002b1912dbe57748817ca8fa9
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-01-25 16:58:30 +0530

    Create minimum array.c

---
+++ minimum array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+ int array[100], minimum,sizec, location=1;
+ printf("Enter the number of element in array"0;
+ scanf("%d",&size);
+ Printf("Enter %d integers \n",size);
+ for(c=1;c< size; c++
+ {
+ if (Array[c]<min)
+ {
+ min=array[c];
+ local=c+1;
+ }
+ }
+ printf("minimum element present at %d %d & its value %d \n", get minimum");
+ return 0;
+ }
