aedcdc3913a107a788fe41ff30af8d5ab4443e72
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-01-25 16:05:58 +0530

    Create multiples.c

---
+++ multiples.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n,d;
+clrscr;
+printf("Enter the limit:");
+scanf("%d",&n);
+printf("\n Enter the number:");
+scanf("%d",&d);
+printf("\n the number divisible by%d are:\n \n",d);
+for(i=1;i<=n;i++)
+if (i%d\t",i);
+getch()
+}
