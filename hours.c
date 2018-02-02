d76e396671ac3f6d0a4a966c93cab27d1df4e1cd
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-02-02 09:43:48 +0000

    Create hours.c

---
+++ hours.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<math.h>
+int min()
+{
+int min=o;
+int hour=0;
+int min=0;
+int minutes to hours=0;
+long local time;
+long time;
+for("Enter local time in minutes");
+scanf("%d",&minutes);
+printf("local time=%d \hours=%d \n min=\n");
+   localTime = time;
+      min = localTime % 60;
+      localTime = localTime / 60;
+       
+      min = localTime % 60;
+       
+       hours = localTime / 60;
+       
+         
+        if(hours > 24)
+          return 0;
+          
+       else
+          return 1;
+              
+    
+       printf("\nTime in Hours is: \n", minToHours); 
+       
+
+   return 0;
+ }
+ 
+ int minToHours(long time1, int* min1, int* hours1)
+
+{
+   long localTime;
+    *min1 = localTime % 60;
+    *hours1 = localTime / 60;
+   localTime *= localTime / 60;
+   return localTime; 
+    }
+
