1ada037b27242ac2d89c6f1dc066d4b7aac5661a
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-02-02 09:21:38 +0000

    Create index element.c

---
+++ index element.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int array[10];
+int i,j,temp;
+printf(Enter the element of an array\n");
+for(i=0;i<10;i++)
+scanf("%d",&array[i]);
+printf(The elements with its index are");
+for(i=0;i<10;i++)
+printf("%d \n",Array[i]);
+}
