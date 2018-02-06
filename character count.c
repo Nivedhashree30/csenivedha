e6cc0f007fb2dbc581dbfb82866c10a64d8b8937
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-02-06 08:58:06 +0000

    Create character count.c

---
+++ character count.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+ char ch;
+ unsigned in long line count;
+ word count, char count;
+ int 0;
+ line count=0
+ word count=0;
+ char count=0;
+ while()ch=getcc(std in);=EOF)
+ {
+ if (ch!='\n')
+ {
+ (++char count)
+ }
+ if(ch=='\n')
+ {
+ ++line count
+ }
+ if(char count>0)
+ {
+ ++word count;
+ ++line count:
+ }
+ print("%\n %ln%ln ln"char count,word,line count);
+ return 0;
+ }
