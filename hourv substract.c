f02271e83c9dec83da981f64113006332d7e136c
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-02-06 08:37:51 +0000

    Create hourv substract.c

---
+++ hourv substract.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+h1=int (raw input())
+m1=int (raw input())
+h2=int (raw input())
+m2=int (raw input())
+h=h1-h2
+m=m1-m2
+if h<0:
+     h=-h
+if m<0:
+     m=-m
+print h,":",m
