88318fbb64e648c73186f284b140bf253d726d99
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-02-06 09:32:09 +0000

    Create space.c

---
+++ space.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i; space;
+char a[30];
+print(Enter the string");
+scanf("%s",a);
+for(i=0;a[0]!="10";i++)
+{
+if(a[i]='')
+{
+ space++
+ }
+ }
+ print("Number of space:%s", sp)
+ print("Number of word in %", space +1);
+ }
