dbaa3598450f021966cbaab2fee9719a87fa3a93
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-02-08 08:40:47 +0000

    Create numeric char.c

---
+++ numeric char.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {
+ char a[20]
+ int flag=0,n,i,a1:
+ clrscr();
+ printf("\n Enter the string language");
+ scanf("%d,&n);
+ printf(Enter the string");
+ scanf("%s",a);
+ for(1=0;i<n;i++)
+ {
+ if ((int a[i]>47&&(int)a[i]<58)
+ flag++
+ }
+ if(flag==0)
+ printf("\n the string has numeric");
+ if(flag>0)
+ printf("The string has %d number",flag)
+ getch()
+ }
