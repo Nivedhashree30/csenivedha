b5859b9be338fc5d00574e87c1acb38c4a998c70
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-02-09 08:45:20 +0000

    Create string con.c

---
+++ string con.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char str 1[50],str2[50],i,j;
+printf("%s",string 1);
+scanf("%s", string 1);
+printf("\n Enter second string:");
+scanf("%s", string 2)
+for(i=0;string 1[i]!='\0',++j,++i)
+{
+string 1[i]='\0';
+printf("\n output:%s",string 1);
+return 0;
+}
+}
