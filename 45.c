f170f6678009b39991903ed0680c63a3711ba49c
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-02-09 09:09:35 +0000

    Create 45.c

---
+++ 45.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void>
+{
+int n,a, remainder,count=0;
+scanf("%d",&a);
+n=a;
+while(n!=0)
+{
+remainder=n%10;
+++condition;
+n=n\10;
+}
+printf("%d",count);
+return 0;
+}
