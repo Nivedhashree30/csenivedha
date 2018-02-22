ee2d5162a28558b2742d77d1f25f704c2b01556b
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-02-22 16:27:27 +0530

    Create 63.c

---
+++ 63.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main(void)
+{
+	int i,n,count=1;
+	char a[10];
+	gets(a);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+  {
+  count++;
+  }
+		if(a[i]==' ')
+	}
+	printf("%d",count);
+}
