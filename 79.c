cb1751175eb1e9c08d07da8d7f2ee9eed61092e8
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-03-08 16:02:14 +0530

    Create 79.c

---
+++ 79.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int n,s;
+printf("\nEnter a numbers");
+scanf("%d",&n);
+s=sqrt(n);
+if(s==(n*n))
+{
+printf("\nYes");
+}
+else
+{
+printf("\nNo");
+return 0
+}
