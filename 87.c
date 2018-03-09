62c8366c5b5b78861bdfe8bbe88aa8d16c6f17a1
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-03-09 10:26:46 +0000

    Create 87.c

---
+++ 87.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+   int x,y,z;
+   printf("Enter The two numbers");
+   scanf("%d%d",&x,&y);
+  if(x>y)
+  z=x;
+  else
+  z=y;
+  while(x!=0&&y!=0)
+    {
+    if(x%z==0&&y%z==0)
+    {
+    break;
+    }
+    else
+    {
+    z=z-1;
+    }
+}
+	printf("The GCD of two number is %d",z);
+}
+
