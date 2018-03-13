643a22dfff354d0883806253f134420cb019f12f
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-03-13 16:31:27 +0530

    Create 105.c

---
+++ 105.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+char=n
+printf("Enter the character:");
+scanf("%c",&a);
+i=a;
+if((a>='A')&&(a<='Z'))
+{
+printf("%d",i-64);
+}
+if((a>='a')&&(a<='z'))
+{
+    printf("%d",i-96);
+    return 0;
+}
+}
